67c5c6d85d25d5a3ead6ed772d4e8c34a02e7ccd
Zetbot Indonesia <zckyachmd@gmail.com>
2024-07-03 19:36:23 +0700

    feat: create contact & show contacts

---
+++ README.md
@@ -0,0 +1,70 @@
+# Address Book
+
+This project is an Address Book designed to manage contacts similarly to Google Contacts (CRM). This website was created as part of the second month's assignment in the bootcamp organized by [Bearmentor Indonesia](https://github.com/bearmentor-community).
+
+## Resources
+
+- **Project Preview:** Currently unavailable as the project is still in development.
+- **Project Logbook:** Accessible through the [Logbook Zacky](https://github.com/zckyachmd/logbook-zacky).
+
+## Technologies Used
+
+- **HTML**
+- **CSS** with Tailwind CSS
+- **JavaScript** (ES6+)
+
+## Features
+- [ ] Accessibility: Ensure your website is accessible to all users.
+- [ ] Responsive Design: Seamlessly adapts to diverse screen sizes.
+- [ ] Navigation: Navigate through the website with ease.
+- [ ] Home: Display a list of contacts.
+- [ ] Add Contact: Add new contacts to your address book.
+- [ ] View Contact: View details of your contacts.
+- [ ] Edit Contact: Edit existing contact information.
+- [ ] Delete Contact: Remove contacts from your address book.
+- [ ] Search Contact: Easily search for contacts.
+
+## Installation
+
+To get a local copy up and running, follow these steps:
+
+<details>
+<summary>Expand for instructions</summary>
+
+### Step
+
+1. Clone the repository.
+
+```bash
+git clone https://github.com/zckyachmd/address-book
+```
+
+2. Navigate to the project directory.
+
+```bash
+cd address-book
+```
+
+3. Install the dependencies.
+
+```bash
+npm install
+```
+
+4. Start the development server.
+
+```bash
+npm run dev
+```
+
+5. Open the `index.html` file in your preferred browser.
+</details>
+
+## License
+
+This project is open source and available under the [MIT License](LICENSE).
+
+## Author
+
+Copyright © 2024 [Zacky](https://zacky.id). Full-Stack Developer and Tech Enthusiast.
+
---
+++ assets/js/storage.js
@@ -0,0 +1,173 @@
+const STORAGE_KEY = "contacts";
+
+/**
+ * Formats the given contact data into a structured format.
+ *
+ * @param {Object} data - The contact data to be formatted.
+ * @return {Object} The formatted contact data.
+ */
+const formatContactData = (data) => {
+  let phones = [];
+  if (data.phone && data.phoneLabel) {
+    phones.push({
+      number: data.phone,
+      label: data.phoneLabel,
+    });
+  }
+
+  let emails = [];
+  if (data.email && data.emailLabel) {
+    emails.push({
+      address: data.email,
+      label: data.emailLabel,
+    });
+  }
+
+  const formattedData = {
+    name: {
+      first: data.firstName || "",
+      middle: data.middleName || "",
+      last: data.lastName || "",
+    },
+    company: {
+      name: data.company || "",
+      jobTitle: data.jobTitle || "",
+    },
+    email: emails,
+    phone: phones,
+    address: {
+      country: data.country || "",
+      streetAddress: data.streetAddress || "",
+      city: data.city || "",
+      postalCode: data.postalCode || "",
+    },
+    notes: data.notes || "",
+  };
+
+  return formattedData;
+};
+
+/**
+ * Generates a unique contact ID by retrieving the saved contacts and incrementing the length by 1.
+ *
+ * @return {number} The generated contact ID.
+ */
+const generateContactId = () => {
+  const savedContacts = getContacts();
+  return savedContacts.length > 0 ? savedContacts.length + 1 : 1;
+};
+
+/**
+ * Saves a contact to local storage.
+ *
+ * @param {Object} data - The contact data to be saved.
+ * @return {Promise<void>} A Promise that resolves when the contact is saved successfully, or rejects with an error if there was an issue.
+ */
+const saveContact = (data) => {
+  try {
+    const formattedData = formatContactData(data);
+
+    const id = generateContactId();
+    formattedData.id = id.toString();
+
+    let savedContacts = getContacts();
+    savedContacts.push(formattedData);
+
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(savedContacts));
+  } catch (error) {
+    console.error("Error saving contact:", error);
+    throw new Error("Failed to save contact. Please try again.");
+  }
+};
+
+/**
+ * Retrieves the list of contacts from local storage.
+ *
+ * @return {Array} An array of contact objects, or an empty array if no contacts are found.
+ */
+const getContacts = () => {
+  return JSON.parse(localStorage.getItem(STORAGE_KEY)) || [];
+};
+
+/**
+ * Retrieves a contact from the list of contacts based on the provided ID.
+ *
+ * @param {string} id - The ID of the contact to retrieve.
+ * @return {Object|undefined} The contact object with the matching ID, or undefined if not found.
+ */
+const getContactById = (id) => {
+  const contacts = getContacts();
+  return contacts.find((contact) => contact.id === id);
+};
+
+/**
+ * Updates a contact in the list of contacts with the provided ID and new data.
+ *
+ * @param {string} id - The ID of the contact to update.
+ * @param {Object} newData - The new data to update the contact with.
+ * @return {Promise<void>} A Promise that resolves when the contact is updated successfully, or rejects with an error if there was an issue.
+ * @throws {Error} If the contact with the provided ID is not found.
+ * @throws {Error} If there was an issue updating the contact. Please try again.
+ */
+const updateContact = (id, newData) => {
+  try {
+    let savedContacts = getContacts();
+
+    // Cari index kontak berdasarkan ID
+    const index = savedContacts.findIndex((contact) => contact.id === id);
+
+    if (index !== -1) {
+      // Update data kontak dengan newData
+      savedContacts[index] = {
+        id: id.toString(),
+        name: {
+          first: newData.firstName || "",
+          middle: newData.middleName || "",
+          last: newData.lastName || "",
+        },
+        company: {
+          name: newData.company || "",
+          jobTitle: newData.jobTitle || "",
+        },
+        email: newData.email || [],
+        phone: newData.phone || [],
+        address: {
+          country: newData.country || "",
+          streetAddress: newData.streetAddress || "",
+          city: newData.city || "",
+          postalCode: newData.postalCode || "",
+        },
+        notes: newData.notes || "",
+      };
+
+      // Simpan kembali ke localStorage
+      localStorage.setItem(STORAGE_KEY, JSON.stringify(savedContacts));
+    } else {
+      throw new Error(`Contact with id ${id} not found.`);
+    }
+  } catch (error) {
+    console.error(`Error updating contact with id ${id}:`, error);
+    throw new Error("Failed to update contact. Please try again.");
+  }
+};
+
+/**
+ * Deletes a contact from the list of saved contacts based on the provided ID.
+ *
+ * @param {string} id - The ID of the contact to delete.
+ * @throws {Error} If there was an issue deleting the contact. Please try again.
+ */
+const deleteContact = (id) => {
+  try {
+    let savedContacts = getContacts();
+
+    // Filter kontak yang ID-nya tidak sesuai dengan yang akan dihapus
+    savedContacts = savedContacts.filter((contact) => contact.id !== id);
+
+    // Simpan kembali ke localStorage
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(savedContacts));
+  } catch (error) {
+    console.error(`Error deleting contact with id ${id}:`, error);
+    throw new Error("Failed to delete contact. Please try again.");
+  }
+};
